1760cd8c8256675e4f3ab0b31f26a4f31dfffd42
cypressg <krasauskas.kipras@gbmail.com>
2025-04-05 18:14:34 +0300

    added initial communication/verbal altercation

--- python/client/src/main.py
+++ python/client/src/main.py
@@ -3,11 +3,14 @@ from socket import AF_INET, SOCK_STREAM, socket
 HOST = "127.0.0.1"
 # Portai iki 1024 reikalauja aukstesniu privilegiju reiktu runnint su sudo, jeigu norim runnint ant port 22. Portas turi sutapt su serverio portu
 PORT = 6969
-BUFF_SIZE = 1024
+BUFF_SIZE = 23
 with socket(AF_INET, SOCK_STREAM) as s:
     s.connect((HOST, PORT))  # prisijungiam prie hosto
-    s.sendall(b"Hello, world")  # nusiunciam teksta
+    s.sendall(b"Zdarof molis")  # nusiunciam teksta
     data = s.recv(
         BUFF_SIZE
     )  # Docs'ai raso - Receive data from the socket. The return value is a bytes object representing the data received. https://docs.python.org/3/library/socket.html#socket.socket.recv
+    print(data)
+    s.sendall(b"pasol naxui")
+    data = s.recv(BUFF_SIZE)
     print(data)
--- python/server/src/main.py
+++ python/server/src/main.py
@@ -3,18 +3,18 @@ from socket import socket, AF_INET, SOCK_STREAM
 
 # [X] Port Binding: Bind to TCP port 22 (or custom port) and start listening.
 
-# [] SYN Reception: Accept incoming SYN packets from clients.
+# [X] SYN Reception: Accept incoming SYN packets from clients.
 
-# [] SYN-ACK Response: Send SYN-ACK with server’s initial sequence number.
+# [X] SYN-ACK Response: Send SYN-ACK with server’s initial sequence number.
 
-# [] ACK Handling: Wait for client’s final ACK to establish the connection.
+# [X] ACK Handling: Wait for client’s final ACK to establish the connection.
 
 
 HOST = "127.0.0.1"
 # Portai iki 1024 reikalauja aukstesniu privilegiju reiktu runnint su sudo, jeigu norim runnint ant port 22
 PORT = 6969
 # Dydzio blokas
-BUFF_SIZE = 1024
+BUFF_SIZE = 12
 """
 AF_INET - Adresu seima (Address Family), kad galetu komunikuoti su IPV4 (0.0.0.0) adresais
 SOCK_STREAM - Nustato, kad tai butu TCP protokolas
@@ -36,18 +36,28 @@ SOCK_STREAM - Nustato, kad tai butu TCP protokolas
 Galimi patobulinimai:
  - Kad servakas neluztu ir suhandlintu jeigu nera duomenu siunciamu is kliento
  - Multi IPv4 ir IPv6 supportas
- 
+ - Butu siaip visai prikolas pasidaryt nuo 0 TCP protokolo handshake, bet sitas dabar eina tai reikia pabaigt;
+
 Su sita komanda galim islupt kompo TCP prisijungimus (daugiau info man netstat)
 netstat -an | grep tcp
+
+
+Pamastymai:
+reiktu paskaityti sita - https://stupidpythonideas.blogspot.com/2013/05/sockets-are-byte-streams-not-message.html
 """
+TEST_LIST = ["TEST", "DUO"]
+my_list = [bytes(item, "utf-8") for item in TEST_LIST]
 with socket(family=AF_INET, type=SOCK_STREAM) as s:
     s.bind((HOST, PORT))  # Bind naudojamas tam, kad priskirtu tinklo sasaja
     s.listen(2)  # Listen duoda leidima klausytis prisijungimu 2-iem vartotojam
     conn, addr = s.accept()  # kimarina ir laukia iki kada ateis duomenys
     with conn:
         while True:
-            data = conn.recv(BUFF_SIZE)  # Paemam buf 1024 byte'u dydzio bloka
+            data = conn.recv(BUFF_SIZE)  # Paemam buf BUFF_SIZE byte'u dydzio bloka
             if not data:  # jei nera nieko iseinam is loop'o servakas issijungia
                 break
-            conn.sendall(data)
+            if data == b"pasol naxui":
+                conn.sendall(b"tu pasol naxui")
+            else:
+                conn.sendall(b"tu normalus?")
             print(data)
